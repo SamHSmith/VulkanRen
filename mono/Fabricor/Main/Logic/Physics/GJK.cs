522844ea4b069e1c30c82873afa1d6dd56ca88a8
Sam Smith <sam@sams-linux-pc.localdomain>
2019-08-10 19:07:01 +0200

    GJK SUCCESS!!!! now epa

--- mono/Fabricor/Main/Logic/Physics/GJK.cs
+++ mono/Fabricor/Main/Logic/Physics/GJK.cs
@@ -59,47 +59,31 @@ namespace Fabricor.Main.Logic.Physics
                 Vector3 AB = insimplex[1] - insimplex[0];
                 Vector3 AC = insimplex[2] - insimplex[0];
                 Vector3 facenormal = Vector3.Cross(AB, AC);
-                Console.WriteLine(Vector3.Cross(facenormal, AC));
-                if (Vector3.Dot(Vector3.Cross(facenormal, AC), -insimplex[0]) > 0)
+                Vector3 toOrigin =-insimplex[0];
+                if (Vector3.Dot(Vector3.Cross(facenormal, AC), toOrigin) > 0)
                 {
-                    if (Vector3.Dot(AC, -insimplex[0]) > 0)
-                    {
-                        newdir = Vector3.Cross(Vector3.Cross(AC, -insimplex[0]), AC);
-                    }
-                    else
-                    {
-                        if (Vector3.Dot(AB, -insimplex[0]) > 0)//Star
-                        {
-                            newdir = Vector3.Cross(Vector3.Cross(AB, -insimplex[0]), AB);
-                        }
-                        else
-                        {
-                            newdir = -insimplex[0];
-                        }
-                    }
+                    newdir = Vector3.Cross(Vector3.Cross(AC, toOrigin), AC);
                 }
                 else
                 {
-                    if (Vector3.Dot(Vector3.Cross(facenormal, AB), -insimplex[0]) > 0)
+                    Vector3 debug = Vector3.Cross(facenormal, -AB);
+                    if (Vector3.Dot(Vector3.Cross(facenormal, -AB), toOrigin) > 0)
                     {
-                        if (Vector3.Dot(AB, -insimplex[0]) > 0)//Star
-                        {
-                            newdir = Vector3.Cross(Vector3.Cross(AB, -insimplex[0]), AB);
-                        }
-                        else
-                        {
-                            newdir = -insimplex[0];
-                        }
+                        newdir = Vector3.Cross(Vector3.Cross(AB, toOrigin), AB);
                     }
                     else
                     {
-                        if (Vector3.Dot(facenormal, -insimplex[0]) > 0)
+                        if (Vector3.Dot(facenormal, toOrigin) > 0)
                         {
                             newdir = facenormal;
                         }
                         else
                         {
                             newdir = -facenormal;
+                            Vector3 b = insimplex[1];
+                            Vector3 c = insimplex[2];
+                            insimplex[1] = c;
+                            insimplex[2] = b;
                         }
                     }
                 }
@@ -122,73 +106,20 @@ namespace Fabricor.Main.Logic.Physics
                 Vector3 adb = Vector3.Normalize(Vector3.Cross(a - d, a - b));
                 float dotadb = Vector3.Dot(adb, -insimplex[0]);
 
-                if (dotabc > 0 && dotabc > dotacd && dotabc > dotadb)
-                {
-                    if (dotacd > dotadb)
-                    {
-                        //abc x acd
-                    }
-                    else
-                    {
-                        //abc x adb
-                    }
-                }
-                else if (dotacd > 0 && dotacd > dotabc && dotacd > dotadb)
-                {
-                    if (dotabc > dotadb)
-                    {
-                        //abc x acd
-                    }
-                    else
-                    {
-                        //abc x adb
-                    }
-                }
-                else if (dotadb > 0 && dotadb > dotabc && dotadb > dotacd)
-                {
-                    if (dotabc > dotacd)
-                    {
-                        //abc x adb
-                    }
-                    else
-                    {
-                        //acd x abd
-                    }
-                }
-                else//inside
+                if (dotabc > 0)
+                    insimplex.RemoveAt(3);
+                else if (dotacd > 0)
+                    insimplex.RemoveAt(1);
+                else if (dotadb > 0)
+                    insimplex.RemoveAt(2);
+                else
                 {
                     outsimplex = insimplex;
-                    if (dotabc > dotacd)
-                    {
-                        if (dotabc > dotadb)
-                        {
-                            newdir = -abc;
-                            return -dotabc;
-                        }
-                        else
-                        {
-                            newdir = -adb;
-                            return -dotadb;
-                        }
-                    }
-                    else
-                    {
-                        if (dotacd > dotadb)
-                        {
-                            newdir = -acd;
-                            return -dotacd;
-                        }
-                        else
-                        {
-                            newdir = -adb;
-                            return -dotadb;
-                        }
-                    }
-
-
+                    newdir = Vector3.UnitY;
+                    return 0.1f;
                 }
-                outsimplex = insimplex;
-                return -1;
+
+                return DoSimplex(insimplex, out outsimplex, out newdir);
             }
         }
     }
